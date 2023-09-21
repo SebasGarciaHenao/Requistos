060ee6fff6d8a530756793987188dc02f44ea4d4
sebas <sebas084garciahenao@gmail.com>
2023-09-20 23:50:06 -0500

    requisitos

--- crear_bar.py
+++ crear_bar.py
@@ -1,5 +1,4 @@
-from crear_usuario import Crear_Usuario
-class Bar(Crear_Usuario):
+class Bar():
     def __init__(self,):
         self.bebida = None
         self.precio = None
@@ -8,35 +7,25 @@ class Bar(Crear_Usuario):
        
     def enlistar(self,bebida,precio):
        
-       usuario = Crear_Usuario
+      
        self.enlistado.append(bebida)
        self.enlistado.append(precio)
        self.total=self.enlistado
-       
+       print (self.enlistado)
 
-       if usuario.Usuario_Crear is not usuario.Usuario_creado:
-           print("El usuario no puede pedir bebidas por no estar registrado")
+      
+            
+     
+
+     
+
+
+
         
-       else:print(self.total)
+
+    
+        
 
  
-     
-      
-      
-usuario = Crear_Usuario("Jorge","Garcia",12)
-usuario.Usuario_Crear()
-usuario.Usuario_creado()
-usuario = Bar()
-usuario.enlistar("Guaro",25000)
 
-       
-print("--------")
-
-      
-usuario = Crear_Usuario("Sebas","Garcia",20)
-usuario.Usuario_Crear()
-usuario.Usuario_creado()
-usuario = Bar()
-usuario.enlistar("Guaro",25000)
-usuario.enlistar("Cerveza",1500)   
        
